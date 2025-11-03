974d9d8c6fd5cd59e3d089131b4c621affcef0ab
Jaime Galan Martinez <xjaime7@gmail.com>
2025-11-03 12:07:17 +0100

    Autoformatted main.py with pep8

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+
 class Recipe(BaseModel):
     id: int
     name: str
@@ -9,18 +10,22 @@ class Recipe(BaseModel):
     instructions: str
     created_at: datetime = datetime.now()
 
+
 app = FastAPI(summary="Mini recipes API")
 
-#In memory storage
+# In memory storage
 recipes_db: list[Recipe] = []
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Mini Recipes API"}
 
+
 @app.get("/api/recipes", response_model=list[Recipe])
 async def get_recipes():
-   return recipes_db
+    return recipes_db
+
 
 @app.get("/api/recipes/{recipe_id}", response_model=Recipe)
 async def get_recipe(recipe_id: int):
@@ -29,15 +34,17 @@ async def get_recipe(recipe_id: int):
             return recipe
     raise HTTPException(status_code=404, detail="Recipe not found")
 
+
 @app.post("/api/recipes", response_model=Recipe)
 async def create_recipe(recipe: Recipe):
     recipes_db.append(recipe)
     return recipe
 
+
 @app.delete("/api/recipes/{recipe_id}")
 async def delete_recipe(recipe_id: int):
-   for i, recipe in enumerate(recipes_db):
+    for i, recipe in enumerate(recipes_db):
         if recipe.id == recipe_id:
             recipes_db.pop(i)
             return {"detail": "Recipe deleted"}
-   raise HTTPException(status_code=404, detail="Recipe not found")
+    raise HTTPException(status_code=404, detail="Recipe not found")
